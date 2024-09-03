3865e4e4cfb0e17680bf25333f2a0a68037f7a70
Sergio Anaya <choptop12@gmail.com>
2024-09-03 10:00:10 -0600

    first commit

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ class BasicTestCase(unittest.TestCase):
     def test_home(self):
         tester = app.app.test_client(self)
         response = tester.get('/', content_type="html/text")
-        self.assertEqual(b'Hello', response.data)
-        self.assertIn(b'Hello', response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Hello', response.data) 
         
 if __name__== '__main__':
     unittest.main()
